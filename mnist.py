0782be0f562c4e76dbe9bc63eaae8771f1458d66
Torak28 <zelechowski28@gmail.com>
2019-10-17 12:45:51 +0200

    One-hot encoder :fire:

--- mnist.py
+++ mnist.py
@@ -1,4 +1,7 @@
 from keras.datasets import mnist
+from keras.layers import Dense
+from keras.models import Sequential
+import keras
 
 # Setup train and test splits
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
@@ -16,5 +19,12 @@ print(f'--------------')
 print(f'After vectorisation:')
 print(f'\tTraining data shape: {x_train.shape}')
 print(f'\tTest data shape: {x_test.shape}')
+
+# One-hot encoded vector
+num_classes = 10
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+print(f'--------------')
+print(f'One- hot encoded vector: {y_train[0]}')
 
 
