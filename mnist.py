98c77a1c49201aaa3485ccf9db1d73222b2bff48
Torak28 <zelechowski28@gmail.com>
2019-10-17 13:27:29 +0200

    Ez function to test!

--- mnist.py
+++ mnist.py
@@ -4,6 +4,24 @@ from keras.models import Sequential
 import matplotlib.pyplot as plt
 import keras
 
+def evaluate(model, batch_size=128, epochs=5, optimizer='sgd', loss_type='mean_squared_error'):
+    model.summary()
+    model.compile(optimizer=optimizer, loss=loss_type, metrics=['accuracy'])
+
+    history = model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, verbose=True, validation_split=.1)
+    loss, accuracy  = model.evaluate(x_test, y_test, verbose=False)
+    plt.plot(history.history['accuracy'])
+    plt.plot(history.history['val_accuracy'])
+    plt.title(f'model accuracy of {epochs} epochs, {batch_size} batch_size, {optimizer} optimizer and {loss_type} loss')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['training', 'validation'], loc='best')
+    plt.show()
+
+    print(f'Test loss: {loss:.3}')
+    print(f'Test accuracy: {accuracy:.3}')
+
+
 # Setup train and test splits
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 print(f'Original data:')
@@ -45,21 +63,6 @@ model = Sequential()
 model.add(Dense(units=32, activation='sigmoid', input_shape=(image_vector_size,)))
 model.add(Dense(units=num_classes, activation='softmax'))
 
-model.summary()
-model.compile(optimizer="sgd", loss='categorical_crossentropy', metrics=['accuracy'])
-
-history = model.fit(x_train, y_train, batch_size=128, epochs=5, verbose=True, validation_split=.1)
-loss, accuracy  = model.evaluate(x_test, y_test, verbose=False)
-
-print(f'{history.history.keys()}')
-
-plt.plot(history.history['accuracy'])
-plt.plot(history.history['val_accuracy'])
-plt.title('model accuracy')
-plt.ylabel('accuracy')
-plt.xlabel('epoch')
-plt.legend(['training', 'validation'], loc='best')
-plt.show()
-
-print(f'Test loss: {loss:.3}')
-print(f'Test accuracy: {accuracy:.3}')
+evaluate(model, batch_size=128, epochs=5, optimizer='sgd', loss_type='mean_squared_error')
+evaluate(model, batch_size=128, epochs=5, optimizer='sgd', loss_type='categorical_crossentropy')
+evaluate(model, batch_size=128, epochs=5, optimizer='adam', loss_type='categorical_crossentropy')
